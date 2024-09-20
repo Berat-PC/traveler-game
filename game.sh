5f85808871960de664a8041a59096c960073e2f2
melolun <melolun@meloluns-Air.lan>
2024-09-20 14:44:59 +0200

    Add info

--- game.sh
+++ game.sh
@@ -3,7 +3,7 @@
 current_path=$(pwd)
 initial_money=510
 # current_money=510
-cd Zurich
+cd Start_point
 
 function check_money {
     # current_money=initial_money - 10
@@ -16,27 +16,50 @@ function check_money {
 }
 
 function check_directory {
-    check_money
-    echo $current_path
+    # check_money
+    echo $PWD
+
+    if  [[ $PWD == "${current_path}/Start_poinr" ]]; then 
+         echo "Start the travel"
+         echo "Chose the city"
+    fi
     
-    if  [[ $current_path == *"Zurich"* ]]; then
-        echo "Story about Zurich"
-        
+    if  [[ $PWD == "${current_path}/Zurich" ]]; then
+        echo "Welcome to Zurich!
+ ____ _   _  ___  ___  ___  _  _ 
+ |_  /| | | || _ \|_ _|/ __|| || |
+  / / | |_| ||   / | || (__ | __ |
+ /___| \___/ |_|_\|___|\___||_||_|
+                                  
+
+Zurich: Switzerland’s largest city, is a dynamic blend of urban sophistication and natural beauty. Set on the shores of Lake Zurich with the snow-capped Alps as a backdrop, the city is a global financial hub that effortlessly balances innovation with a deep sense of history. Its charming old town is filled with medieval streets, boutique shops, and vibrant cafes, while its world-class museums and galleries highlight a rich cultural heritage. Known for its pristine lifestyle, Zurich also boasts a lively arts and nightlife scene, making it a city where tradition and modernity seamlessly coexist."
+       cd ${current_path}/Start_point  
     fi
 
-    if  [[ $current_path == *"Basel"* ]]; then
-        echo "Story about Basel"
-        
+    if  [[ $PWD == *"Basel"* ]]; then
+        echo "
+ | _ )  /_\  / __|| __|| |   
+ | _ \ / _ \ \__ \| _| | |__ 
+ |___//_/ \_\|___/|___||____|"
+ cd ${current_path}/Start_point 
     fi
 
-    if  [[ $current_path == *"Bern"* ]]; then
-        echo "Story about Bern"
-        
+    if  [[ $PWD == *"Bern"* ]]; then
+        echo "Welcome to BERN
+
+ | _ )| __|| _ \| \| |
+ | _ \| _| |   /| .` |
+ |___/|___||_|_\|_|\_| 
+    cd ${current_path}/Start_point 
     fi
 
-    if  [[ $current_path == *"Lugano"* ]]; then
-        echo "Story about Bern"
-        
+    if  [[ $PWD == *"Lugano"* ]]; then
+        echo "Welcome to LUGANO !
+  _    _   _   ___    _    _  _   ___  
+ | |  | | | | / __|  /_\  | \| | / _ \ 
+ | |__| |_| || (_ | / _ \ | .` || (_) |
+ |____|\___/  \___|/_/ \_\|_|\_| \___/ 
+        cd ${current_path}/Start_point 
     fi
     
  
